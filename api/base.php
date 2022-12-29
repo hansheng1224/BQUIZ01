19aa11be7cdbad851eca1f7f18bd975887f9a075
hansheng1224 <hansheng1224@gmail.com>
2022-12-30 00:38:40 +0800

    finish all()

---
+++ api/base.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+class DB{
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db12";
+    protected $table;
+    protected $pdo;
+
+    public function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+  
+    public function find($id){
+        $sql="SELECT * FROM $this->table";
+        if(is_array($id)){
+            // foreach($id as $key=>$value){
+            //     $tmp[]="`$key`=`$value`";
+            // }
+            $tmp=$this->arrayToSqlArray($id);
+            $sql=$sql." WHERE ".join(" && ",$tmp);
+        }else{
+            $sql=$sql." WHERE `id`='$id'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function all(...$arg){
+        $sql="select * from $this->table";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->arrayToSqlArray($arg[0]);
+                $sql=$sql." where ".join(" && ",$tmp);
+            }else{
+                $sql=$sql.$arg[0];
+            }
+        }
+        if(isset($arg[1])){
+            $sql=$sql.$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function save(){
+
+    }
+
+    public function del(){
+
+    }
+
+    public function sum(){
+
+    }
+
+    public function count(){
+
+    }
+
+    public function max(){
+
+    }
+
+    public function min(){
+
+    }
+
+    public function avg(){
+
+    }
+
+    private function arrayToSqlArray($array){
+        foreach($array as $key=>$value){
+            $tmp[]="`$key`=`$value`";
+        }
+        return $tmp;
+    }
+}
+
+function dd(){
+
+}
+
+function to(){
+
+}
+
+function q(){
+
+}
+
+$db=new DB('bottom');
+$bot=$db->all();
+print_r($bot);
+
+?>
---
+++ db04.sql
@@ -0,0 +1,268 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-12-29 09:23:05
+-- 伺服器版本： 10.4.24-MariaDB
+-- PHP 版本： 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db04`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ad`
+--
+
+CREATE TABLE `ad` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `ad`
+--
+
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES
+(1, '轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動G', 1),
+(5, '轉知:教育是人類升沉的樞紐-2013教師生命成長營D', 1),
+(6, '木火日尸木', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `acc` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pw` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`) VALUES
+(1, 'admin', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bottom`
+--
+
+CREATE TABLE `bottom` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `bottom` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bottom`
+--
+
+INSERT INTO `bottom` (`id`, `bottom`, `price`) VALUES
+(1, '2023 科技大學版權所有', 100),
+(3, '2022頁尾版權', 200),
+(4, '2022頁尾版權', 300);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `image`
+--
+
+INSERT INTO `image` (`id`, `img`, `sh`) VALUES
+(1, '01D05.jpg', 1),
+(2, '01D07.jpg', 0),
+(3, '01D08.jpg', 0),
+(4, '01D03.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `mvim`
+--
+
+CREATE TABLE `mvim` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `mvim`
+--
+
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
+(1, '01C01.gif', 1),
+(3, '01C03.gif', 0),
+(4, '01C02.gif', 1),
+(5, '01C04.gif', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `news`
+--
+
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES
+(3, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 0),
+(4, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表', 1),
+(5, '11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場\r\n舉辦「高中職生涯輔導知能研習」\r\n中區學校每校至多2名\r\n以普通科、專業類科教師優先報名參加\r\n生涯規劃教師次之，參加人員公差假\r\n並核實派代課\r\n當天還有專車接送(8:35前在員林火車站集合)\r\n如此好康的機會，怎能錯過？！\r\n熱烈邀請師長們向輔導室(分機234)報名\r\n名額有限，動作要快！！\r\n報名截止日期：本周四 10月25日17:00前！', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `title`
+--
+
+CREATE TABLE `title` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `title`
+--
+
+INSERT INTO `title` (`id`, `img`, `text`, `sh`) VALUES
+(7, '01B02.jpg', 'AAAAAA', 1),
+(9, '01B01.jpg', 'DDDDDD', 0),
+(13, '01B03.jpg', '木日火木尸', 0),
+(14, '01B01.jpg', '火日尸火日', 0);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `ad`
+--
+ALTER TABLE `ad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `bottom`
+--
+ALTER TABLE `bottom`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `mvim`
+--
+ALTER TABLE `mvim`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `title`
+--
+ALTER TABLE `title`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
+--
+ALTER TABLE `ad`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
+--
+ALTER TABLE `bottom`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `mvim`
+--
+ALTER TABLE `mvim`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `title`
+--
+ALTER TABLE `title`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
